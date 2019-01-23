96926486a4cdad8da46f67d03c8afc290c4f7c16
softprops <d.tangren@gmail.com>
2019-01-22 20:15:10 -0500

    simplify

--- src/path.ts
+++ src/path.ts
@@ -1,5 +1,5 @@
 export function translatePath(apiGatewayPath: string): string {
     return apiGatewayPath
         .replace(/{proxy\+}/g, '*')
-        .replace(/({)([^}]+)(})/g, ':$2');
+        .replace(/{([^}]+)}/g, ':$1');
 }
