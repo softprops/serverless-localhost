6ae8bf0ceb877dfe117b8bd40f65157f38cb972e
softprops <d.tangren@gmail.com>
2019-01-24 23:34:51 -0500

    screenshot

--- integration-tests/proxyplus.js
+++ integration-tests/proxyplus.js
@@ -1,6 +1,8 @@
 module.exports.handler = (event, context, callback) => {
+  console.log('received event');
+  console.log(event);
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({greeting: `Hello ${event.path}`})
+    body: JSON.stringify({ greeting: `Hello ${event.path}` })
   });
-}
+};
